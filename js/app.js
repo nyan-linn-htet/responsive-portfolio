a3ad4add6ebac4c719077909ba1ac02b31b7a592
Nyan Linn Htet <nyanlinhtet2301@gmail.com>
2022-06-06 14:23:07 +0630

    modified toggle & add dark-mode

--- js/app.js
+++ js/app.js
@@ -24,4 +24,35 @@ $(function() {
             $("nav").css("box-shadow", "none");
         }
     })
-});
+
+    // Skills
+
+    $(".skills-text").on("click", function() {
+        let parent = $(this).parent();
+        $(".up", parent).toggleClass("rotate");
+        $(".language", parent).slideToggle("0.3s");
+    })
+
+    // Services
+
+    $(".service-btn").on("click", function() {
+        let parent = $(this).parent().parent();
+        $(".service-model", parent).addClass("show-model");
+    });
+    $(".service-close").on("click", function() {
+        $(".service-model").removeClass("show-model");
+    })
+
+    // Dark-Mode 
+
+    $(".dark").on("click", function() {
+        $(".dark").toggleClass("hide");
+        $(".light").toggleClass("hide");
+        $("body").toggleClass("dark-mode");
+    })
+    $(".light").on("click", function() {
+        $(".light").toggleClass("hide");
+        $(".dark").toggleClass("hide");
+        $("body").toggleClass("dark-mode");
+    })
+})
